000765d496d3212b31a3084c4b7eaf8767f7a243
Yazhong Liu <l900422@vip.qq.com>
2014-04-25 23:19:50 +0800

    Initial Commit

---
+++ TODO/TDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDAppDelegate.h
+//  TODO
+//
+//  Created by Yorkie on 4/25/14.
+//  Copyright (c) 2014 Yorkie Neil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TODO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TODO
+//
+//  Created by Yorkie on 4/25/14.
+//  Copyright (c) 2014 Yorkie Neil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDAppDelegate class]));
+  }
+}
